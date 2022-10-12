cfe1a7b39aa58f7a0fe9ecf3e62206c6f2f897cf
NateLakai <106267621+Plague3Dr2K@users.noreply.github.com>
2022-10-13 01:53:45 +0800

    added-new-project

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,11 +5,16 @@ export class Pros extends Component {
   render() {
     return (
       <div id="projects" className='container-fluid'>
-        <div className='row justify-content-center'>
-        <div className='quote col-lg-4'>
+        <div className='row'>
+        <div className='quote col-lg-6'>
         <p className='framecap'>Random Quote Machine</p>
         <iframe className='frame'src="https://plague3dr2k.github.io/nate-quote-machine/"><p className='framecap'>Random Quote Machine</p></iframe>
         <p className='info'>Made with ReactJs and SASS</p>
+      </div>
+      <div className='quote col-lg-6'>
+        <p className='markdown'>Markdown previewer</p>
+        <iframe className='frame2'src="https://plague3dr2k.github.io/nate-markdown-previewer/"><p className='framecap'>Random Quote Machine</p></iframe>
+        <p className='info'>Made with ReactJs and Styled-components</p>
       </div>
        </div>
       </div>
